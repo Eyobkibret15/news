204dfd8e1efa60c9105be736ad128bb3c74192ff
Eyob Kibret <eyob.kibret@northvolt.pl>
2021-12-29 11:42:20 +0100

    page br

--- News/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-"""
-WE GATHER ALL DATABASE MODULE 
-"""
-class NewsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'News'
